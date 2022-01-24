a4f35ef99c25d222d1027cdd690cbb3f4f2ae506
Elham Ahmadzadeh <eliahm63@gmail.com>
2022-01-24 23:28:36 +0100

    g

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@ function showAmountValue() {
   calculateMonthyCost()
 
  amountSlider.oninput = function() {
+  var value = (this.value-this.min)/(this.max-this.min)*100
   this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + value + '%, #fff ' + value + '%, white 100%)'
 };
 
