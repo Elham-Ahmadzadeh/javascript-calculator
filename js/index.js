6770988d0eca489a8f8773c0eb06acdbfc0144ed
Elham Ahmadzadeh <eliahm63@gmail.com>
2022-01-25 00:40:58 +0100

    fixed

--- js/index.js
+++ js/index.js
@@ -12,21 +12,32 @@ loanAmount.textContent = parseInt(amountSlider.value)
 loanPeriod.textContent = parseInt(periodSlider.value)
 amountSlider.addEventListener('input', showAmountValue, false)
 function showAmountValue() {
-  loanAmount.innerHTML =  parseInt(amountSlider.value).toLocaleString('fr-FR')
+  loanAmount.innerHTML = parseInt(amountSlider.value).toLocaleString('fr-FR')
   calculateMonthyCost()
- amountSlider.oninput = function() {
-  this.style.background =  `linear-gradient(to right, #6b1fae 0%, #6b1fae ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 100%)`
-};
+  loanAmount.textContent = amountSlider.value
+  let bulletPosition = amountSlider.value / amountSlider.max
+  loanAmount.style.left = bulletPosition * 40 + 'em'
+  amountSlider.oninput = function () {
+    this.style.background = `linear-gradient(to right, #6b1fae 0%, #6b1fae ${
+      ((this.value - this.min) / (this.max - this.min)) * 100
+    }%, #DEE2E6 ${
+      ((this.value - this.min) / (this.max - this.min)) * 100
+    }%, #DEE2E6 100%)`
+  }
 }
 periodSlider.addEventListener('input', showPeriodValue, false)
 function showPeriodValue() {
   loanPeriod.textContent = parseInt(periodSlider.value)
-  let bulletPosition = (periodSlider.value / periodSlider.max);
-  loanPeriod.style.left = (bulletPosition * 7) + "em";
+  let bulletPosition = periodSlider.value / periodSlider.max
+  loanPeriod.style.left = bulletPosition * 17 + 'em'
   calculateMonthyCost()
-  periodSlider.oninput = function() {
-    this.style.background =  `linear-gradient(to right, #6b1fae 0%, #6b1fae ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 100%)`
-  };
+  periodSlider.oninput = function () {
+    this.style.background = `linear-gradient(to right, #6b1fae 0%, #6b1fae ${
+      ((this.value - this.min) / (this.max - this.min)) * 100
+    }%, #DEE2E6 ${
+      ((this.value - this.min) / (this.max - this.min)) * 100
+    }%, #DEE2E6 100%)`
+  }
 }
 
 function calculateMonthyCost() {
